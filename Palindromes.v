43843d806691e09eafd82eda7e95d70e34e7fff1
Jack Gallagher <jack@gallabytes.com>
2015-02-27 16:30:44 -0800

    Clean palindromes
    Relations are reliably better ideas than functions, not sure how I
    didn't think of this earlier

--- Palindromes.v
+++ Palindromes.v
@@ -13,108 +13,75 @@ Inductive pal {X} : list X -> Prop :=
 
 Hint Constructors pal.
 
-Theorem pal_l_plus_rev_l : forall X (l : list X), pal (l ++ rev l).
-Proof. induction l; crush.
-       - Case "a :: l". rewrite app_assoc; constructor; assumption.
-Qed.
-(** [] *)
-
 (** **** Exercise: 5 stars, optional (palindrome_converse) *)
 (** Using your definition of [pal] from the previous exercise, prove
     that
      forall l, l = rev l -> pal l.
 *)
 
-Hint Resolve f_equal.
+Inductive init_and_last {X : Type} : list X -> list X -> X -> Prop :=
+| ising : forall x, init_and_last [x] [] x
+| icons : forall x y ixs xs, init_and_last xs ixs x ->
+                             init_and_last (y :: xs) (y :: ixs) x.
 
-Hint Rewrite app_inv_tail.
-Hint Rewrite app_inj_tail.
+Hint Constructors init_and_last.
 
-Fixpoint init_and_last {X : Type} (l : list X) : option (list X * X) :=
-  match l with
-    | [] => None
-    | x :: xs => match init_and_last xs with
-                  | None => Some ([], x)
-                  | Some (xs', x') => Some (x :: xs', x')
-                end
-  end.
-
-Hint Resolve f_equal.
-
-Theorem init_and_list : forall X xs ys (y : X), init_and_last xs = Some (ys, y) ->
-                                           xs = ys ++ [y].
-Proof with crush.
-  induction xs as [|x xs'].
-  - inversion 1.
-  - intros. simpl in *. destruct (init_and_last xs') as [p|] eqn:ilxs'.
-    + Case "Some p". destruct p; invert H...
-    + Case "None". inverts H; destruct xs'; trivial.
-      simpl in *; destruct (init_and_last xs'); try destruct p; inverts ilxs'.
+Theorem idec {X} (l : list X) : {l = []} + {exists xs x, init_and_last l xs x}.
+  induction l; [left; reflexivity|right].
+  - destruct IHl; [subst|destr_prods]; eauto.
 Qed.
 
-Hint Rewrite init_and_list.
+Lemma init_and_list : forall X xs ys (y : X),
+                        init_and_last xs ys y ->
+                        xs = ys ++ [y].
+  induction 1; crush. Qed.
 
-Definition lengthOrder {X : Type} (ls1 ls2 : list X) :=
-  length ls1 < length ls2.
+Definition lengthOrder {X : Type} (ls1 ls2 : list X) := length ls1 < length ls2.
 
 Hint Unfold lengthOrder.
+Hint Unfold well_founded.
 Hint Constructors Acc.
 
 Lemma lengthOrder_wf' : forall X len (ls : list X), 
                           length ls <= len -> Acc lengthOrder ls.
-Proof. unfold lengthOrder; induction len; crush. Qed.
+  unfold lengthOrder; induction len; crush. Qed.
+
+Local Hint Resolve lengthOrder_wf'.
 
 Theorem lengthOrder_wf : forall X, well_founded (@lengthOrder X).
-Proof. red. intros. eapply lengthOrder_wf'. eauto. Qed.
+  red; eauto. Qed.
 
 Hint Resolve lengthOrder_wf.
 
-Lemma le_S : forall n m, n <= m -> S n <= S m.
-  induction 1; crush. Qed.
-
-Lemma le_Sn_m : forall n m, S n <= m -> n <= m.
-  induction 1; crush. Qed.
-
 Hint Rewrite app_length.
 
-Theorem init_and_last_length_ordered : forall X xs ys (x y : X),
-           init_and_last (x :: xs) = Some (ys, y) ->
-           length ys <= length (x :: xs).                              
-Proof. induction xs; intros; [crush|].
-       - simpl in *. destruct (init_and_last xs) eqn:ilxs.
-         + destruct p; apply init_and_list in ilxs; inverts H; crush.
-         + inverts H; crush.
-Qed.
+Lemma init_and_last_length_ordered X xs ys : forall y : X,
+                                               init_and_last xs ys y ->
+                                               length ys <= length xs.
+  induction 1; crush. Qed.
 
 Hint Resolve init_and_last_length_ordered.
 
-Theorem deq_induction X (P : list X -> Prop)
-        (Pi : P [])
-        (Psi : forall x, P [x])
-        (Psnoc : forall x xs y, P xs -> P (x :: xs ++ [y])) :
-  (forall l, P l).
-Proof. apply well_founded_induction with (R := lengthOrder); eauto.
-       - introv IHlo. destruct x as [|x xs']; eauto.
-         + destruct (init_and_last xs') as [p|] eqn:ilxs; [destruct p|]; eauto.
-           * apply init_and_list in ilxs. subst. apply Psnoc. apply IHlo.
-             unfold lengthOrder; crush.
-           * destruct xs'; crush. destruct (init_and_last xs'); [destruct p|]; crush.
+Theorem deq_induction : forall X (P : list X -> Prop),
+                          P []
+                       -> (forall x, P [x]) 
+                       -> (forall x xs y, P xs -> P (x :: xs ++ [y]))
+                       -> forall l, P l.
+  introv Pnil Psi PCons;
+  apply well_founded_induction with (R := lengthOrder); [auto|intros xs IHlo].
+  - destruct xs as [|x xs']; [assumption|].
+    + destruct (idec xs') as [|H]; [crush|destr_prods].
+      * rewrite (init_and_list H) in *; apply PCons; apply IHlo; red; crush.
 Defined.
 
+Theorem pal__rev : forall X (l : list X), pal l -> rev l = l.
+  induction 1; eauto; simpl; rewrite rev_app_distr.
+  - apply (f_equal (fun l => x :: l ++ [x])); apply IHpal.
+Qed.
+
+Hint Resolve app_inv_tail.
 Hint Rewrite rev_unit.
 
-Theorem pal__rev : forall X (l : list X), pal l -> l = rev l.
-Proof. induction 1; eauto. simpl; rewrite rev_unit. crush. Qed.
-
-Hint Resolve pal__rev.
-Hint Resolve app_inv_tail.
-
-Theorem rev__pal : forall X (l : list X), l = rev l -> pal l.
-Proof with crush. intro X.
-  refine (deq_induction (fun xs => xs = rev xs -> pal xs)
-                        (fun _ => palnil)
-                        (fun x _ => palsing x)
-                        _)...
-  - Case "x :: xs ++ [y]". replace (rev xs) with xs in *. intuition.
-    + SCase "assertion". eapply app_inv_tail; eassumption.
+Theorem rev__pal : forall X (l : list X), rev l = l -> pal l.
+  intro; eapply (deq_induction (fun xs => rev xs = xs -> pal xs)); ecrush.
 Qed.
